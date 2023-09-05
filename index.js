d73b2c10f908dd24392a6f9efa390965f065248e
Hayden Roney <roneyhayden@gmail.com>
2023-09-05 12:29:05 -0500

    Lab finished

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name);
+}
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name);
+}
+function appendCat(name) {
+    const catsCopy = [...cats];
+    catsCopy.push(name);
+    return catsCopy;
+}
+function prependCat(name) {
+    const catsCopy = [...cats];
+    catsCopy.unshift(name);
+    return catsCopy;
+}
+function removeFirstCat(name) {
+    const catsCopy = [...cats];
+    catsCopy.shift(name);
+    return catsCopy;
+}
+function removeLastCat(name) {
+    const catsCopy = [...cats];
+    catsCopy.pop(name);
+    return catsCopy;
+}
